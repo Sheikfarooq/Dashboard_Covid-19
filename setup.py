3a2ede5a406233eeb55eeaa317a3f3f6fd810c9f
Sheikfarooq <farooqppap@gmail.com>
2022-07-24 16:35:34 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DashBoard_covid-19',
+    author='Farooq_Sheik',
+    license='',
+)
